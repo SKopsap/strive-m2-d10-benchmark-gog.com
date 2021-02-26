7d60e89b322cacb3d7e8ad5db1850221b2dd0dae
Sean Knowles <s@seanknowles.dev>
2021-02-26 11:34:32 +0000

    initial commit, app structure, directory structure and component files created

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-// my-first-snowpack/index.js
 import './css/index.scss'
-
-import { helloWorld } from './scripts/hello-world.js'
-
-document.getElementById('changeTitleBtn').addEventListener('click', () => {
-  console.log('clicked')
-})
